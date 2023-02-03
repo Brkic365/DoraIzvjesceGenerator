6a1a23875911fcfb110c940fefd29fbd43d3e1e4
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2023-02-03 19:19:37 +0100

    ae

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,30 +9,37 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const code = req.body.code || "";
+  if (code.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please enter a valid code.",
+      },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: `${code}""" \nThis arduino code`,
+      temperature: 0,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ['"""'],
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+
+    res.status(200).json({ result: response });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,8 +48,8 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
